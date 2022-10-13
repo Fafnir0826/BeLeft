00ccec721a4e78afc3e158cf3c33744c10a79ebb
Fafnir <hector880826@gmail.com>
2022-10-13 15:49:12 +0800

    Get Data From CharacterData_SO.cs

--- Assets/Scripts/MonoBehavior/CharacterStates.cs
+++ Assets/Scripts/MonoBehavior/CharacterStates.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class CharacterStates : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public CharacterData_SO characterData;
 
-    // Update is called once per frame
-    void Update()
+    #region  Read from Data_SO
+    public int maxHealth
     {
-        
+        get { if (characterData != null) return characterData.maxHealth; else return 0; }
+        set { characterData.maxHealth = value; }
     }
+    public int currentHealth
+    {
+        get { if (characterData != null) return characterData.currentHealth; else return 0; }
+        set { characterData.currentHealth = value; }
+    }
+    public int maxHV
+    {
+        get { if (characterData != null) return characterData.maxHV; else return 0; }
+        set { characterData.maxHV = value; }
+    }
+    public int currentHV
+    {
+        get { if (characterData != null) return characterData.currentHV; else return 0; }
+        set { characterData.currentHV = value; }
+    }
+    public int maxSP
+    {
+        get { if (characterData != null) return characterData.maxSP; else return 0; }
+        set { characterData.maxSP = value; }
+    }
+    public int currentSP
+    {
+        get { if (characterData != null) return characterData.currentSP; else return 0; }
+        set { characterData.currentSP = value; }
+    }
+    #endregion
 }
