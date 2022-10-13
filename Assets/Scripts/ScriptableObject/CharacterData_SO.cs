de5fb697bc19465e255e525afbdb235148b66662
Fafnir <hector880826@gmail.com>
2022-10-13 14:46:53 +0800

    Add PlayerData(HealthPoint,HungerPoint,StaminaPoint )

--- Assets/Scripts/ScriptableObject/CharacterData_SO.cs
+++ Assets/Scripts/ScriptableObject/CharacterData_SO.cs
@@ -6,6 +6,14 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Data", menuName = "Character Stats/Data")]
 public class CharacterData_SO : ScriptableObject
 {
-    
+    [Header("State Info")]
+    public int maxHealth;
+    public int currentHealth;
+    public int  maxHV;  //最大飢餓度HungerValue
+    public int currentHV;  //當前飢餓度HungerValue
+    public int  maxSP;  //最大耐力值
+    public int  currentSP;  //當前耐力值
+
+
 }
 
