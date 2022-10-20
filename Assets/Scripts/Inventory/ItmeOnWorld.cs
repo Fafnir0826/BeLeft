34fc4fa93427dac71a75b0738eb707555da6d5b0
Fafnir <hector880826@gmail.com>
2022-10-20 18:15:56 +0800

    change folder and import other assetpack and revive inventoryabout scripts

--- Assets/Scripts/Inventory/ItmeOnWorld.cs
+++ Assets/Scripts/Inventory/ItmeOnWorld.cs
@@ -7,6 +7,16 @@ public class ItmeOnWorld : MonoBehaviour
     public Item thisItem;
     public Inventory playerInvnentory;
 
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Bag"))
+        {
+            AddNewItem();
+            Destroy(gameObject);
+        }
+    }
+
     public void AddNewItem()
     {
         if(!playerInvnentory.itemLists.Contains(thisItem))
