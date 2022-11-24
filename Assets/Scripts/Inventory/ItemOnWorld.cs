63cf4db71c2b36f33b3d625b5b998c2aa983f5b8
Fafnir <hector880826@gmail.com>
2022-11-24 20:07:09 +0800

    scriptobjects setting

--- Assets/Scripts/Inventory/ItemOnWorld.cs
+++ Assets/Scripts/Inventory/ItemOnWorld.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
+[RequireComponent(typeof(XRGrabInteractable))]
 public class ItemOnWorld : MonoBehaviour
 {
     public Item thisItem;
